b31ae7aa81d42e87a55b4d621cfb120c77e14838
SADIO_Stéphane <78489573+StephaneSaddio@users.noreply.github.com>
2021-04-24 16:01:25 +0200

    Update __init__.py

--- FourierDrawing/__init__.py
+++ FourierDrawing/__init__.py
@@ -2,6 +2,5 @@
 __version__ = "0.0.1"
 
 from .ImageManipulation.Imgmanip import Imagemanip
-from .FourierApproximation.Fourier_Approx import Fourier_Approx
+from .FourierApproximation.class_fourierApproximation import Fourier_Approx
 from .Circles_radii_center.ComplexCircles import Circles
-from .Animate_FT.animate import Animate
