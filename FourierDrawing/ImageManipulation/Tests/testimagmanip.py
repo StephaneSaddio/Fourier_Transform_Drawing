249f768391ba1a16a7c0966ef19e6fcd2e037005
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-04-24 13:06:54 +0200

    Create imagemanip test

---
+++ FourierDrawing/ImageManipulation/Tests/testimagmanip.py
@@ -0,0 +1,9 @@
+import sys
+import unittest
+sys.path.append('..')
+from ImageManipulation.Imgmanip import Imagemanip
+
+url = 'https://www.seekpng.com/png/detail/116-1164659_line-drawing-bunny-rabbit-at-getdrawings-bunny-drawing.png'
+
+class test_Imgmanip(unittest.TestCase):
+
