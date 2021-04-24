6f5331f68c3c9e487bed590b618870eeda20fb42
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-04-24 13:09:10 +0200

    testing convert_binary function

--- FourierDrawing/ImageManipulation/Tests/testimagmanip.py
+++ FourierDrawing/ImageManipulation/Tests/testimagmanip.py
@@ -19,3 +19,15 @@ class test_Imgmanip(unittest.TestCase):
         img.convert_binary(scale=3, thresh_val=200)
         self.assertGreater(img.thresh_val, 0) # thresh_val > 0
         self.assertLess(img.thresh_val, 255) # thresh_val < 255
+
+    def test_convert_binary (self):
+        """ assert that the image array is binary with 0 (black) and 255 (white) """
+        img = Imagemanip(url)
+        img.single_color()
+        img.convert_binary(scale=3, thresh_val=200)
+        for i in range(len(img.image_array)):
+            for j in range(len(img.image_array[0])):
+                self.assertIn(img.image_array[i][j], (1,255)) # = 0 or 255
+ 
+if __name__ == '__main__':
+    unittest.main()
