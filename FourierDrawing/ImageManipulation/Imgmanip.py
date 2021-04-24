0a85bd4e2405c31f22c2826f35c28fffe0361be1
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-04-24 13:00:51 +0200

    Add  functions

--- FourierDrawing/ImageManipulation/Imgmanip.py
+++ FourierDrawing/ImageManipulation/Imgmanip.py
@@ -75,3 +75,97 @@ class Imagemanip:
         print("The image size is : {}".format(self.img_blackwhite.size))
         print("The image mode is : {}".format(self.img_blackwhite.mode))
         print("Numbre of pixels is: {}".format(self.pixels.sum()))
+        
+    def distance_matrix(self):
+        """ Get non-zero pixel coordiantes than calcualte distance between each pair of them """
+
+        # Find positions of non-zero pixels
+        non_zero_P_index = np.where(self.pixels_vector > 0)[0]
+
+        # Make a range array index form 1 to len(non_zero_P_index)
+        N = len(non_zero_P_index)+1
+        arrange_index = np.array(range(1, N ))
+        
+        # Replace each non-zero pixel with its number
+        self.flat_img_mod = deepcopy(self.pixels_vector)
+        for r, pix in enumerate(non_zero_P_index):
+            self.flat_img_mod[pix] = r+1
+
+        # Get coordiantes for each non-zero pixel
+        img_idx = np.reshape(self.flat_img_mod, self.pixels.shape)
+        self.coord_list = []
+        for v in arrange_index:
+            v_coords = tuple([int(i) for i in np.where(img_idx==v)])
+            self.coord_list.append(list(v_coords))
+        
+        # Calcualte distance between each pair of coords
+        self.distance_matrix = distance.cdist(self.coord_list, self.coord_list, 'euclidean')
+   
+    def contours_search(self, plot=True):
+        """ Get the image tour using the nearest neighbor heuristic  """ 
+
+        edges = self.coord_list
+        length_edges = len(edges)
+
+        # Set a random starting edge form length edges
+        start = int(np.random.choice(range(length_edges),size=1))
+
+        # Set the starting edge for heuristic nearest neighbor research
+        tour = [start]
+        current_edge = start
+
+        # Look for the point closest to the current edge
+        for step in range(0, length_edges):
+            dist_row = deepcopy(self.distance_matrix[current_edge,:])
+            for i in tour:
+                dist_row[i] = np.inf
+            nearest_neighbor = np.argmin(dist_row)
+            if nearest_neighbor not in tour:
+                tour.append(nearest_neighbor)
+            current_edge = nearest_neighbor
+        
+        # Extract the coordinates of the tour points 
+        x_y_tour = list()
+        for i in range(length_edges+1):
+            v = edges[tour[i % length_edges]]  
+            x_y_tour.append(v)
+        xy_tour = np.array(x_y_tour)
+
+        x_tour = xy_tour[:,1]
+        y_tour = -xy_tour[:,0]
+
+        x_tour = x_tour - x_tour[0]
+        y_tour = y_tour - y_tour[0]
+
+        # Close the circuit by returning to the beginning point
+        np.append(x_tour, x_tour[0])
+        np.append(y_tour, y_tour[0])
+        length_edges = length_edges + 1
+    
+        self.x_tour = x_tour
+        self.y_tour = y_tour
+        self.length_pixels = length_edges
+
+        # Show the image countour
+        if plot:
+            plt.plot(self.x_tour, self.y_tour)
+
+    def get_splines(self, degree=1, plot=True):
+        """ Smooth the curves tour angles  """
+
+        # Smooth the curves tour angles
+        length_pixels_list = list(range(0,self.length_pixels))
+        x_spl = UnivariateSpline(length_pixels_list, self.x_tour, k=degree)
+        y_spl = UnivariateSpline(length_pixels_list, self.y_tour, k=degree)
+        
+        self.x_spl = x_spl
+        self.y_spl = y_spl
+        
+        if plot:
+            vect = np.linspace(0, self.length_pixels-1, 1000)
+            x_cord = list()
+            y_cord = list()
+            for v in vect:
+                x_cord.append(x_spl(v))
+                y_cord.append(y_spl(v))  
+            p = plt.plot(x_cord,y_cord)
