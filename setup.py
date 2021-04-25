23b51bcac76911521b3ae384d4d0a54427ef8baf
SADIO_Stéphane <78489573+StephaneSaddio@users.noreply.github.com>
2021-04-25 16:32:57 +0200

    Upload to github

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   description='Fourier drawing',
   url='https://github.com/KamalAyoubi/Fourier_Transform_Drawing',
   author='Stephane SADIO, Kamal AYOUBI and Otmane JABBAR'
-  author_email=['stephane.sadio@etu.umontpellier.fr','kamal.ayoubi@etu.umontpellier.fr','otmane.jabbar@etu.umontpellier.fr']
+  author_email=['stephane.sadio@etu.umontpellier.fr','kamal.ayoubi@etu.umontpellier.fr','otmane.jabbar@etu.umontpellier.fr'],
   license='MIND',
   packages=['FourierDrawing','FourierDrawing.ImageManipulation', 'FourierDrawing.FourierApproximation',
    'FourierDrawing.Circles_radii_center','FourierDrawing.Animate_FT'],
