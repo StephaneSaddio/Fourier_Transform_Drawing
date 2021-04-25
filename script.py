5085f3847b31b03ab216e7466c0fdbab66c1bc4f
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-04-25 14:58:25 +0200

    update scripte

--- script.py
+++ script.py
@@ -1,36 +1,29 @@
-#%%
 from FourierDrawing.ImageManipulation.Imgmanip import Imagemanip
 from FourierDrawing.FourierApproximation.Class_fourierApproximation import FourierApprox
 from FourierDrawing.Circles_radii_center.ComplexCircles import Circles
-#from FourierDrawing.Animate_FT import Animate
-#from Animate_FT import Animate 
-#%%
+from FourierDrawing.Animate_FT import Animate
 
+# Import raw image
 url = 'https://www.seekpng.com/png/detail/116-1164659_line-drawing-bunny-rabbit-at-getdrawings-bunny-drawing.png'
-
-
 img = Imagemanip(url)
+
+# Show raw image, his format, size and mode. 
 img.show()
 
-
-#%%
+#Convert image to single color, Berarize it then convert to black and wight
 img.single_color()
 img.convert_binary(scale=3, thresh_val=200)
 img.black_and_white()
 
+# Show black and white image 
 img.show_black_and_white()
-# %%
+
+# Get non-zero pixel coordiantes than calcualte distance between each pair of them
 img.distance_matrix()
 
-#%%
+# Show the image countour, then the splines curves image.
 img.contours_search(plot=True)
-# %%
-
 img.get_splines(plot=True)
-#%%
-ani = animation.FuncAnimation(fig, animate, frames=num_frames,
-                              interval=interval, blit=True, init_func=init)
-#%%
 
-ani.save('./Images/animation5.gif', writer='imagemagick')
+# Show gif 
 DisplayImage(url='./Images/animation5.gif')
